4fb9233cacb649fe259aeef8ac054db318ad76cc
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-03-11 18:00:05 +0900

    2024.03.11

---
+++ 9012.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+sum = 0
+stack = []
+last = 0
+num = []
+
+for i in range(N):
+    
+
+for i in range(N):
+    A = int(sys.stdin.readline())
+    if last < A:
+        for _ in range(A):
+            print("+")
+        
