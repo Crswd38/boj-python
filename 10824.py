2fdadafbd5fad91ede4a5899f375be42db502e23
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-03-19 17:59:08 +0900

    2024.03.19

--- 10824.py
+++ 10824.py
@@ -1,17 +1,4 @@
 import sys
 
-str = sys.stdin.readline()
-j = 0
-result = ""
-
-for i in str:
-    if i.isalpha():
-        j = ord(i)+13
-        if j > 122:
-            j -= 26
-        elif j > 90 and j <= 103:
-            j -= 26
-        result += chr(j)
-    else:
-        result += i
-print(result)
+a, b, c, d = sys.stdin.readline().split()
+print(int(a + b) + int(c + d))
