74e308ab5d4001942efe5f77fcfb5a667ee5c20d
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-05-14 18:04:51 +0900

    2024.05.14

--- solution.py
+++ solution.py
@@ -1,2 +1,16 @@
-a = "asd123"
-print(a[1:5:2])
+def solution(s):
+    result = ""
+    for i in s.split():
+        for j in range(len(i)):
+            if j%2:
+                result += i[j].lower()
+            else:
+                result += i[j].upper()
+        result += " "
+    return result
+
+
+
+print(solution("  TRy HElLo  WORLD "))
+# print(solution([[57, 192, 534, 2], [9, 345, 192, 999]]))
+# print(solution([[1, 2], [3, 4]]))
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-def solution(intStrs, k, s, l):
-    result = []
-    for i in intStrs:
-        i = str(i)
-        if int(i[s:s+l]) > k:
-            result.append(int(i[s:s+l]))
-    return result
+a = int(input())
+c = int(input())
 
-print(solution(["0123456789","9876543210","9999999999999"],50000,5,5))
+b_square = c - a
+print(b_square)
