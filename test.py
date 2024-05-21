38dd0f428ce47c2fc124dcf31a1d75f9b46d7fcb
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-05-21 17:13:56 +0900

    2024.05.21

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-a = int(input())
-c = int(input())
+a, b, c, d, e, f = map(int, input())
+# 1 3 -1 4 1 7
+x = a+b
+y = b+c
+z = c+f
 
-b_square = c - a
-print(b_square)
+print()
+
+# x + 3y = -1
+# 4x + y = 7
+
+# 5x + 4y = 6
+
+# -3x + 2y = -8
