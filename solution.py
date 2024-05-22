9c9d95e8d7b69934f6d89e7fb8d059c4bb185047
Crswd38 <105415745+Crswd38@users.noreply.github.com>
2024-05-22 17:56:59 +0900

    2024.05.22

--- solution.py
+++ solution.py
@@ -1,16 +1,5 @@
-def solution(s):
-    result = ""
-    for i in s.split():
-        for j in range(len(i)):
-            if j%2:
-                result += i[j].lower()
-            else:
-                result += i[j].upper()
-        result += " "
-    return result
-
-
-
-print(solution("  TRy HElLo  WORLD "))
-# print(solution([[57, 192, 534, 2], [9, 345, 192, 999]]))
-# print(solution([[1, 2], [3, 4]]))
+n, m = map(int, input().split())
+sum = n
+for i in range(1, m):
+    sum *= n
+    print(str(sum)[-1])
--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-a, b, c, d, e, f = map(int, input())
-# 1 3 -1 4 1 7
-x = a+b
-y = b+c
-z = c+f
 
-print()
-
-# x + 3y = -1
-# 4x + y = 7
-
-# 5x + 4y = 6
-
-# -3x + 2y = -8
+arr = []
+sum = 0
+T = int(input())
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    
